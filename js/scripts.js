a2b05ec1e168b56c6fcfeee6f6c36ac05f3c6519
nicolas.srurpiselli <nsrurpiselli@gmail.com>
2022-07-28 20:22:45 -0300

    desafio

--- js/scripts.js
+++ js/scripts.js
@@ -41,3 +41,33 @@ function talles (cantidadTalle) {
         return 'No hay talle'
     }
 }
+class producto{
+    constructor(id, nombre, precio, stock){
+        this.id = id
+        this. nombre = nombre
+        this. precio = precio
+        this. stock = stock
+        
+        }
+}
+
+const producto1 = new Producto(1, "zapatillas", 10000, 20)
+const producto2 = new Producto(2, "buzo", 7000, 20)
+const producto3 = new Producto(3, "remeras", 4000, 20)
+const producto4 = new Producto(4, "medias", 1000, 20)
+
+let productos = [producto1, producto2, producto3, producto4]
+const sectionProductos = document. getElementById("card_productos_section").
+
+productos.forEach(product => {
+    sectionProductos.innerHTML += `
+    <div class="card_productos" id="productos${product.id}"style="width: 18rem;">
+        <div class="card-body">
+            <h5 class="card-title">${product.nombre}</h5>
+            <p class="card-text">Precio: $${product.precio}</p>
+            <p class="card-text">Stock: ${product.stock}</p>
+        </div>
+    </div>
+    `
+}
+)
